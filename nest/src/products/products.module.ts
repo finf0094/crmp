6f54ad0e1fec0738738a08ad387387b432eeee78
finf0094 <finf0094@gmail.com>
2024-02-25 14:34:18 +0500

    fix: products module

--- nest/src/products/products.module.ts
+++ nest/src/products/products.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { PrismaService } from '@prisma/prisma.service';
+import { S3Service } from '@s3/s3.service';
 
 @Module({
     controllers: [ProductsController],
-    providers: [ProductsService, PrismaService],
+    providers: [ProductsService, PrismaService, S3Service],
 })
 export class ProductsModule {}
